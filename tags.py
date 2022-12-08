09693c4645c6fca2a9bbf7a44f6966a6ab9f1530
chanxxx1912 <109349749+chanxxx1912@users.noreply.github.com>
2022-12-08 16:54:31 +0530

    Update tags.py

--- tags.py
+++ tags.py
@@ -3,145 +3,147 @@ import json
 session = boto3.Session(profile_name='default')
 client = session.client('ecs')
 
-
 #Listing the clusters by its names
 cluster_list = client.list_clusters()
 cluster_name = cluster_list["clusterArns"]
-for i in cluster_name:
-   cluster_name=i
+for name in cluster_name:
+   cluster_name = name
+   cluster_short_name = (name.split('/')[1])
+   
    #print(cluster_name)
    #Printing the tag present in cluster
-   response = client.list_tags_for_resource(
+   tags_inside_cluster = client.list_tags_for_resource(
         resourceArn=cluster_name
    )
-   print(response['tags'])
+   print(cluster_short_name , tags_inside_cluster['tags'])
 
-   #Listing the task inside the cluster 
+   #Listing task ----> 19,20 line is according to doc variable name cant be changed AttributeError:
    task_list = client.list_tasks(
-     cluster=(cluster_name)
+    cluster=(cluster_name)
    )
-   task = task_list
-   if task == task_list:
-    task =  (task_list['taskArns'])
-    for i in task:
-        tag_in_task = i
-        task= (i.split('/')[2])
-        #print(task)
-        #Printing the tags present in the task 
-        response = client.list_tags_for_resource(
-            resourceArn = tag_in_task
-            
-        )
-        print(response['tags'])
-        describe_task = client.describe_tasks(
-          cluster = (cluster_name),
-          tasks =[
-            task
-          ]
-        ) 
-        #print(describe_task)
-               
-        
-        output = (json.dumps(describe_task,indent=4,default=str)) 
-        report = open('test.json', 'w')
-        report.write((json.dumps(describe_task,indent=4,default=str)))
-        report.close()
-        with open ('test.json') as f:
-          data_file = json.load(f)
-        containers = [] 
-       
-        tasks = data_file['tasks']
-        for task in tasks:
-          containers = task['containers']
-          lastStatus = containers
-          taskDefinitionArn = task
-          
-          RUNNING = lastStatus
+   task_arn_inside_task_list =(task_list['taskArns'])
 
-          for container in containers:
-            if lastStatus == RUNNING :
-              print(container['name'])
-              print(container['image'])
-              print(container['lastStatus'])
-              #print(task['taskDefinitionArn'])
-              #printing the tags present in taskdefination
-              response = client.list_tags_for_resource(
-                resourceArn=(task['taskDefinitionArn'])
-              ) 
-              print(response['tags']) 
-                 
---------------------------------------------------------------------------------------------------------------------------------
-Output 
-PS E:\7-12> python 8-12.py
-[{'key': 'ecs', 'value': 'instance'}, {'key': 'aws', 'value': 'resource'}]
-[{'key': 'value', 'value': 'micro'}, {'key': 'aws:ecs:clusterName', 'value': 'env'}]
+   for elements_in_task_list in task_arn_inside_task_list:
+    task_arn = (elements_in_task_list)
+    #print(task_arn)
+    
+    task_name = (elements_in_task_list.split('/')[1])
+        
+    task_id = (elements_in_task_list.split ('/')[2]) 
+        
+    #tags
+    tags_inside_task = client.list_tags_for_resource(
+        resourceArn = task_arn
+    )
+    print(task_name ,tags_inside_task['tags'])
+
+    #Describe task
+    describe_task = client.describe_tasks(
+        cluster = (cluster_name),
+        tasks =[
+            task_id
+        ]
+
+    )
+    output = (json.dumps(describe_task,indent=4,default=str)) 
+    report = open('test.json', 'w')
+    report.write((json.dumps(describe_task,indent=4,default=str)))
+    report.close()
+    with open ('test.json') as f:
+        data_file = json.load(f)
+    containers = []   
+    
+    tasks = data_file['tasks']
+    
+    for task_details in tasks:
+        containers = task_details['containers']
+        
+        
+        for container in containers:
+         print(container['name'])  
+         print(container['image'])  
+         print(container['lastStatus'])
+         
+
+         tags_in_describe_task =  client.list_tags_for_resource(
+            resourceArn=(task_details['taskDefinitionArn'])
+         )
+         print( container['name'],tags_in_describe_task['tags'])
+        
+      
+------------------      ------------------------------------------------------------------------------------------
+output
+PS E:\7-12> python new.py 
+env [{'key': 'ecs', 'value': 'instance'}, {'key': 'aws', 'value': 'resource'}]
+env [{'key': 'value', 'value': 'micro'}, {'key': 'aws:ecs:clusterName', 'value': 'env'}]
 redis1
 redis:7.0.5
 RUNNING    
-[{'key': 'us-east-1', 'value': 'us-east-2'}, {'key': 'usa', 'value': 'france'}]
-[{'key': 'aws:ecs:clusterName', 'value': 'env'}, {'key': 'pod ', 'value': 'cast'}]
+redis1 [{'key': 'us-east-1', 'value': 'us-east-2'}, {'key': 'usa', 'value': 'france'}]
+env [{'key': 'aws:ecs:clusterName', 'value': 'env'}, {'key': 'pod ', 'value': 'cast'}]
 redis2
 redis:7.0.5-bullseye
 RUNNING
-[{'key': 'output ', 'value': 'value'}, {'key': 'parameter', 'value': 'json'}]
-[{'key': 'environment ', 'value': 'green'}, {'key': 'aws:ecs:clusterName', 'value': 'env'}]
+redis2 [{'key': 'output ', 'value': 'value'}, {'key': 'parameter', 'value': 'json'}]
+env [{'key': 'environment ', 'value': 'green'}, {'key': 'aws:ecs:clusterName', 'value': 'env'}]
 deployment
 nginx:1.23.2
 RUNNING     
-[{'key': 'syntax', 'value': 'hello'}, {'key': 'print', 'value': 'helloworld'}]
+deployment [{'key': 'syntax', 'value': 'hello'}, {'key': 'print', 'value': 'helloworld'}]
 nginx     
 nginx:1.23
 RUNNING   
-[{'key': 'syntax', 'value': 'hello'}, {'key': 'print', 'value': 'helloworld'}]
-[{'key': 'veg', 'value': 'carrot'}, {'key': 'fruit', 'value': 'apple'}]
-[{'key': 'country ', 'value': 'india'}, {'key': 'aws:ecs:clusterName', 'value': 'prod'}]
+nginx [{'key': 'syntax', 'value': 'hello'}, {'key': 'print', 'value': 'helloworld'}]
+prod [{'key': 'veg', 'value': 'carrot'}, {'key': 'fruit', 'value': 'apple'}]
+prod [{'key': 'country ', 'value': 'india'}, {'key': 'aws:ecs:clusterName', 'value': 'prod'}]
 testing
 nginx:1.22-alpine
 RUNNING
-[{'key': 'jbl', 'value': 'go'}, {'key': 'sound', 'value': 'noise'}]
-[{'key': 'skills', 'value': 'art'}, {'key': 'aws:ecs:clusterName', 'value': 'prod'}]
+testing [{'key': 'jbl', 'value': 'go'}, {'key': 'sound', 'value': 'noise'}]
+prod [{'key': 'skills', 'value': 'art'}, {'key': 'aws:ecs:clusterName', 'value': 'prod'}]
 production
 nginx:latest
 RUNNING     
-[{'key': 'name', 'value': 'task'}, {'key': 'image', 'value': 'pic'}]
-[{'key': 'morals', 'value': 'value'}, {'key': 'aws:ecs:clusterName', 'value': 'prod'}]
+production [{'key': 'name', 'value': 'task'}, {'key': 'image', 'value': 'pic'}]
+prod [{'key': 'morals', 'value': 'value'}, {'key': 'aws:ecs:clusterName', 'value': 'prod'}]
 c2
 nginx:1.22.1-alpine-perl
 RUNNING
-[{'key': 'needs', 'value': 'food'}, {'key': 'shelter', 'value': 'building'}]
+c2 [{'key': 'needs', 'value': 'food'}, {'key': 'shelter', 'value': 'building'}]
 c1
 redis:6-bullseye
 RUNNING
-[{'key': 'needs', 'value': 'food'}, {'key': 'shelter', 'value': 'building'}]
-[{'key': 'region', 'value': 'ap-northeast-1'}, {'key': 'south', 'value': 'ap-southeast-1'}]
-[{'key': 'aws:ecs:clusterName', 'value': 'dev'}, {'key': 'aggrement ', 'value': 'bond'}]
+c1 [{'key': 'needs', 'value': 'food'}, {'key': 'shelter', 'value': 'building'}]
+dev [{'key': 'region', 'value': 'ap-northeast-1'}, {'key': 'south', 'value': 'ap-southeast-1'}]
+dev [{'key': 'aws:ecs:clusterName', 'value': 'dev'}, {'key': 'aggrement ', 'value': 'bond'}]
 image2
 nginx:stable
 RUNNING
-[{'key': 'bond ', 'value': 'hopes'}, {'key': 'family', 'value': 'villan'}]
+image2 [{'key': 'bond ', 'value': 'hopes'}, {'key': 'family', 'value': 'villan'}]
 image1
 redis:7-alpine
 RUNNING
-[{'key': 'bond ', 'value': 'hopes'}, {'key': 'family', 'value': 'villan'}]
-[{'key': 'container', 'value': 'image'}, {'key': 'aws:ecs:clusterName', 'value': 'dev'}]
+image1 [{'key': 'bond ', 'value': 'hopes'}, {'key': 'family', 'value': 'villan'}]
+dev [{'key': 'container', 'value': 'image'}, {'key': 'aws:ecs:clusterName', 'value': 'dev'}]
 devcontainer
 redis:latest
 RUNNING
-[{'key': 'cluster ', 'value': 'task'}, {'key': 'name ', 'value': 'dev'}]
-[{'key': 'aws:ecs:clusterName', 'value': 'dev'}, {'key': 'docker', 'value': 'hub'}]
+devcontainer [{'key': 'cluster ', 'value': 'task'}, {'key': 'name ', 'value': 'dev'}]
+dev [{'key': 'aws:ecs:clusterName', 'value': 'dev'}, {'key': 'docker', 'value': 'hub'}]
 containerc1
 nginx:perl
 RUNNING
-[{'key': 'city', 'value': 'mumbai'}, {'key': 'capital', 'value': 'delhi'}]
-              
-              
-
-   
-
-
-   
-
-       
+containerc1 [{'key': 'city', 'value': 'mumbai'}, {'key': 'capital', 'value': 'delhi'}]
+        
  
 
+    
+    
+  
+  
+
+
+ 
    
+
+
